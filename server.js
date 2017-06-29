f4b648db3ed0ce9ada69235ecfd84a8895216fdb
brbernardo90 <brbernardo90@gmail.com>
2017-06-28 22:14:45 -0300

    config heroku 1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 // var port = 80;
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
   console.log('Express server listening on port: ' + port);
